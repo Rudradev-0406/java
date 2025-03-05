09fca1d014a5bb0dd8b3a2862391d1da2490d1e9
RUDRADEV-DEWANGAN-06 <rudradev.dewangan2023@ssipmt.com>
2025-03-05 22:47:14 +0530

    Add files via upload

---
+++ java_apna collage/string.java
@@ -0,0 +1,26 @@
+public class string{
+    public static void main(String[] args) {
+        // // concatenate
+        // String name1 ="raju";
+        // String name2 ="prahsd";
+        // String name3= name1+" and "+name2;
+        // System.out.println(name3);
+        // System.out.println(name3.charAt(2));
+        // System.out.println(name3.length());
+
+
+        // ***********************************************************************************************************************************
+
+
+        // String name = "rudra";
+        // name = name.replace("rudra", "dev"); 
+        // System.out.println(name);
+        String name= "rudradev";
+        System.out.println(name.substring(0,4));
+        String name2 = name.replace('d','A');
+        System.out.println(name2);
+        System.out.println(name);
+
+    }
+
+}
